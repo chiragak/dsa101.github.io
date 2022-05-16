f18aef116b686996c1dbba4b611625d05467a28c
Chirag A K <chiragajekar@gmail.com>
2022-05-16 19:50:06 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld{
     public static void main(String args[]){
         System.out.print("Hello World");
+        //println prints the new line at the end
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+
     }
 }
