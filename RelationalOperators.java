d409d320f43541fa1c2a775f6c7cbe4f4dbd9cf4
Chirag A K <chiragajekar@gmail.com>
2022-05-18 00:05:44 +0530

    .

---
+++ RelationalOperators.java
@@ -0,0 +1,11 @@
+public class RegionalLogicalOperators {
+    public static void main(String[] args){
+        int a=10,b=10;
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+    }
+}
