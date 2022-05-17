c8ef384bcc4cf7a29cc01d7ba1d7a5f26152cafe
Chirag A K <chiragajekar@gmail.com>
2022-05-18 00:34:35 +0530

    Update LogicalOperators.java

--- LogicalOperators.java
+++ LogicalOperators.java
@@ -1,11 +1,13 @@
 public class LogicalOperators {
     public static void main(String[] args){
-        // AND 
         int a,b;
         a=10;
         b=10;
+        //AND
         System.out.println(a>10 && b>10);
+        //OR
         System.out.println(a>10 || b>10);
+        //NOT
         System.out.println(!(a>b));
     }
 }
