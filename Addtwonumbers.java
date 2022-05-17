76747ef391023144eabfa8d5b8d4b5588be7793d
Chirag A K <chiragajekar@gmail.com>
2022-05-17 14:53:00 +0530

    Create Addtwonumbers.java
    Taking inputs and adding two numbers

---
+++ Addtwonumbers.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Addtwonumbers {
+        public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int a,b;
+        a=s.nextInt();
+        b=s.nextInt();
+        int c=a+b;
+        System.out.println(c);
+    }
+    
+}
