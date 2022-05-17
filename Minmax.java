6b638a86a469c42be3549ad15e77194bbca87928
Chirag A K <chiragajekar@gmail.com>
2022-05-17 22:10:22 +0530

    Program to check minimum and max value of the datatype

---
+++ Minmax.java
@@ -0,0 +1,9 @@
+public class Minmax {
+    public static void main(String[] args){
+        int min=Integer.MIN_VALUE;
+        System.out.println(min);
+        int max=Integer.MAX_VALUE;
+        System.out.println(max);
+    }
+    
+}
