d2d8aa46b5d39c76e6f50ac79e17c5844b5b176e
Chirag A K <70628014+chiragak@users.noreply.github.com>
2023-07-15 10:27:20 +0530

    .
    .

--- HelloWorld.java
+++
@@ -1,9 +0,0 @@
-public class HelloWorld{
-    public static void main(String args[]){
-        System.out.print("Hello World");
-        //println prints the new line at the end
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-
-    }
-}
