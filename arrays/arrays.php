d31f9254a0e301d3ac408025ac9eda739a450364
Chirag A K <70628014+chiragak@users.noreply.github.com>
2023-09-11 23:58:41 +0530

    Update arrays.php

--- arrays/arrays.php
+++ arrays/arrays.php
@@ -5,7 +5,11 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<<<<<<< Updated upstream:arrays/arrays.php
         <link rel="stylesheet" href="../assets/css/style.css">
+=======
+        <link rel="stylesheet" href="/includes/style.css">
+>>>>>>> Stashed changes:arrays.php
       
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.24.1/themes/prism.min.css" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.24.1/prism.min.js"></script>
